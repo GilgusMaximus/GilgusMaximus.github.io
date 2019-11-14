19068fc8e613dfaf655a8231f3bda0abea30c924
Luca <hohmann.luca@tutanota.com>
2019-11-14 18:14:42 +0100

    test for a homework

--- javascript/collapsibles.js
+++ javascript/collapsibles.js
@@ -18,7 +18,7 @@ function collapse(){
             if (content.style.display === "block") {
                 content.style.display = "none";
             } else {
-                content.style.display = "block";
+                content.style.display = "hallo";
             }
         });
     }
