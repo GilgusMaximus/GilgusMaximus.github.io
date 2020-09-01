c12841ab82744f89e27ae3e4e19a37a737deab65
Luca <hohmann.luca@tutanota.com>
2020-09-01 20:15:38 +0200

    a Zoom, Animations, Icons Added: 1)Zoom - when hovering over icons in triple col, they zoom in 2) Animation - Basics for animations added 3) Icons - Added new Icons for University and Software projects

--- javascript/menuButton.js
+++ javascript/menuButton.js
@@ -3,20 +3,26 @@ let headerBar = null;
 let menuButton = null;
 
 window.onload = function () {
-    headerBar = this.document.getElementById("headerBar");
-    menuButton = this.document.getElementById("menuButton");
-    menuButton.style.display = "none";
+    body1 = this.document.getElementById("body1");
+    body2 = this.document.getElementById("body2");
+    //menuButton.style.display = "none";
 }
 
 
-
+function on() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  } 
 
 function showMenu(){
-    menuButton.style.display = "block";
-    headerBar.style.display = "none";
+    body2.style.display = "block";
+    body1.style.display = "none";
 }
 
 function showBar(){
-    menuButton.style.display = "block";
-    headerBar.style.display = "none";
+    body1.style.display = "block";
+    body2.style.display = "none";
 }
