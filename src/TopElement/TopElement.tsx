e3e107ee08964f90ed33a0036c6b1a17b26c57f0
Luca Hohmann <lprogramming@tutanota.com>
2022-03-16 16:07:54 +0200

    New stuff

--- src/TopElement/TopElement.tsx
+++ src/TopElement/TopElement.tsx
@@ -9,7 +9,7 @@ function TopElement() {
           <div className="base">
             <img src={logo} className="App-logo" alt="logo" />
             <h1>Luca Hohmann</h1>
-            <h2 className="Info-Me">Studying M.Sc. Information Security, Privacy, Web Technology, Gamification @ TUM</h2>
+            <h2 className="Info-Me">Studying M.Sc. Information Security, Privacy, <br className='breakh'></br>Web Technology, Gamification @ TUM</h2>
             <p className='Pre-Quote'>Why I want to work in my specialization areas:</p>
             <p className='Quote'>"All practically relevant problems are theoretically solved. [...]<br/>
               Nonetheless the practical state of IT-Security is disastrous"<br/>
